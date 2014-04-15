763c9b987d3e0870d7433742713546354ec156f5
Sydney Richardson <sydneyrichardson3@gmail.com>
2014-04-15 14:13:00 -0400

    created Settings View Controller

---
+++ Lecture Loot/Lecture Loot/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  Lecture Loot
+//
+//  Created by Alejandro Fernandez on 4/15/14.
+//  Copyright (c) 2014 CashU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
---
+++ Lecture Loot/Lecture Loot/SettingsViewController.m
@@ -0,0 +1,24 @@
+//
+//  SettingsViewController.m
+//  Lecture Loot
+//
+//  Created by Alejandro Fernandez on 4/15/14.
+//  Copyright (c) 2014 CashU. All rights reserved.
+//
+
+#import "SettingsViewController.h"
+
+@interface SettingsViewController ()
+
+@end
+
+@implementation SettingsViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
